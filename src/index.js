80053679eb6e31bdc8de7c7a1f27bcdb9e95ddad
medaminedev66 <md.amine.smahi@gmail.com>
2021-09-07 16:21:07 +0100

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.html';
 import './style.css';
-
+import { getMenuData, displayMenuItems } from './getAPI.js';
 import generatePopup from './modules/comments.js';
 import mmeal from './img/meal.jpg';
 
@@ -11,13 +11,12 @@ const meal = {
   strMeal: 'Teriyaki Chicken Casserole',
 };
 
+const getDataAPI = 'https://www.themealdb.com/api/json/v1/1/search.php?f=e';
+
 document.querySelector('.test-comment').addEventListener('click', () => {
   generatePopup(meal);
 });
 
-import { getMenuData, displayMenuItems } from './getAPI.js';
-
-const getDataAPI = 'https://www.themealdb.com/api/json/v1/1/search.php?f=e';
-
-getMenuData(getDataAPI).then((meals) => { displayMenuItems(meals); });
-
+getMenuData(getDataAPI).then((meals) => {
+  displayMenuItems(meals);
+});
