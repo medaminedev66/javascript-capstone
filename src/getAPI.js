0dd3b279b0b9bcbb1a8ef0265950a270b740b0a0
ridaarif98 <ridaarif16@gmail.com>
2021-09-07 15:30:56 +0200

    Display api data in home page

--- src/getAPI.js
+++ src/getAPI.js
@@ -3,8 +3,29 @@ const getMenuData = async (url) => {
   return score.json();
 };
 
-const displayMenuItems = ((meals) => {
-  console.log(meals.meals);
-});
+const displayMenuItems = (meals) => {
+  const mealsArray = meals.meals;
+  mealsArray.forEach((m) => {
+    const li = document.createElement('li');
+    li.className = 'menuItem';
+    const image = document.createElement('img');
+    image.src = m.strMealThumb;
+    li.appendChild(image);
+    const para = document.createElement('p');
+    para.innerText = m.strMeal;
+
+    const icon = document.createElement('i');
+    icon.className = 'far fa-heart';
+    para.appendChild(icon);
+    li.appendChild(para);
+
+    const btn = document.createElement('button');
+    btn.innerText = 'Comment';
+    btn.id = m.idMeal;
+
+    li.appendChild(btn);
+    document.getElementById('menuList').appendChild(li);
+  });
+};
 
 export { getMenuData, displayMenuItems };
