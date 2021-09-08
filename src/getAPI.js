254f1c36742a71569c4abd4346ad7b0c7c4064bf
ridaarif98 <ridaarif16@gmail.com>
2021-09-08 13:39:47 +0200

    Store meals in loacl storage and update like icon

--- src/getAPI.js
+++ src/getAPI.js
@@ -2,16 +2,40 @@ import { likeMeal, getLikes, displayLikes } from './likeItems.js';
 import generatePopup from './modules/comments.js';
 
 const involvementAPI = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
-const appId = 'oj8lCVSvVKOoKDOy43br';
+const appId = 'mCi78cjOxZzyvn3BuMkU';
 
 const getMenuData = async (url) => {
   const score = await fetch(url);
   return score.json();
 };
 
+const setItem = (id, isLiked) => {
+  if (localStorage.getItem('Liked') == null) {
+    localStorage.setItem('Liked', JSON.stringify([]));
+  }
+  const likedItems = JSON.parse(localStorage.getItem('Liked'));
+  likedItems.push({
+    liked: isLiked,
+    mealId: id,
+  });
+  localStorage.setItem('Liked', JSON.stringify(likedItems));
+};
+
+const updateItem = (id, isLiked) => {
+  const likedItems = JSON.parse(localStorage.getItem('Liked'));
+  likedItems.forEach((item) => {
+    if (item.mealId === id) {
+      item.liked = isLiked;
+    }
+  });
+  localStorage.setItem('Liked', JSON.stringify(likedItems));
+};
+
 const displayMenuItems = (meals) => {
   const mealsArray = meals.meals;
   mealsArray.forEach((m) => {
+    let isLiked = false;
+    setItem(m.idMeal, isLiked);
     const li = document.createElement('li');
     li.className = 'menuItem';
     const image = document.createElement('img');
@@ -21,10 +45,10 @@ const displayMenuItems = (meals) => {
     para.innerText = m.strMeal;
 
     const aTag = document.createElement('a');
+    aTag.id = m.idMeal;
 
     const icon = document.createElement('i');
     icon.className = 'far fa-heart';
-    icon.id = m.idMeal;
     aTag.appendChild(icon);
     para.appendChild(aTag);
     li.appendChild(para);
@@ -42,19 +66,34 @@ const displayMenuItems = (meals) => {
     li.appendChild(btn);
     document.getElementById('menuList').appendChild(li);
 
-    // likeMeal(involvementAPI, appId, m.idMeal);
-    getLikes(involvementAPI, appId).then((data) => {
-      const numlikes = displayLikes(m.idMeal, data);
-      if (numlikes) {
-        spans.innerText = numlikes;
-        aTag.classList = 'likeIcon';
-      }
-    });
+    if (isLiked === false) {
+      aTag.classList.remove('likeIcon');
+    } else {
+      aTag.classList.add('likeIcon');
+    }
+
     aTag.addEventListener('click', () => {
+      if (isLiked === false) {
+        isLiked = true;
+        aTag.classList.add('likeIcon');
+        updateItem(m.idMeal, isLiked);
+      } else {
+        isLiked = false;
+        aTag.classList.remove('likeIcon');
+        updateItem(m.idMeal, isLiked);
+      }
+
       likeMeal(involvementAPI, appId, m.idMeal);
       getLikes(involvementAPI, appId).then((data) => {
         spans.innerText = displayLikes(m.idMeal, data);
       });
+    });
+
+    getLikes(involvementAPI, appId).then((data) => {
+      const numlikes = displayLikes(m.idMeal, data);
+      if (numlikes) {
+        spans.innerText = numlikes;
+      }
     });
   });
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './index.html';
 import './style.css';
 import { getMenuData, displayMenuItems } from './getAPI.js';
 
-const getDataAPI = 'https://www.themealdb.com/api/json/v1/1/search.php?f=e';
+const getDataAPI = 'https://www.themealdb.com/api/json/v1/1/search.php?f=c';
 
 getMenuData(getDataAPI).then((meals) => {
   displayMenuItems(meals);
