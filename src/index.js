e280a659b5d785d682298f42525dd74d5117866b
ridaarif98 <ridaarif16@gmail.com>
2021-09-09 13:30:10 +0200

    Add test for homepage counter and style items

--- src/index.js
+++ src/index.js
@@ -1,52 +1,72 @@
 import './index.html';
 import './style.css';
 import { getMenuData, displayMenuItems } from './getAPI.js';
-import countMenuItems from './modules/counter.js';
+import countMenuItems from './counter.js';
 
 const getDataAPI = 'https://www.themealdb.com/api/json/v1/1/';
-const allItems = document.getElementById('menuCounter');
-const indianFood = document.getElementById('indianFood');
-const seaFood = document.getElementById('seaFood');
-const homeItems = document.getElementById('menuList');
-const indianItmes = document.getElementById('indianList');
-const seaItems = document.getElementById('seaList');
+const main = document.querySelector('.homeCounter');
+const homeList = document.querySelector('.homeList');
+const allItems = document.querySelector('.menuCounter');
+const indianFood = document.querySelector('.indianFood');
+const seaFood = document.querySelector('.seaFood');
+const homeItems = document.querySelector('.menuList');
+const indianItmes = document.querySelector('.indianList');
+const seaItems = document.querySelector('.seaList');
 
-allItems.addEventListener('click', () => {
-  indianItmes.style.display = 'none';
-  seaItems.style.display = 'none';
-  getMenuData(`${getDataAPI}search.php?f=b`).then((meals) => {
-    displayMenuItems(meals, homeItems);
-  });
+getMenuData(`${getDataAPI}search.php?f=b`).then((meals) => {
+  displayMenuItems(meals, homeList);
 });
 
 getMenuData(`${getDataAPI}search.php?f=b`).then((meals) => {
-  const itemsLength = countMenuItems(meals);
-  displayMenuItems(meals, homeItems);
+  const mealsA = meals.meals;
+  const itemsLength = countMenuItems(mealsA);
   allItems.innerText = `Home (${itemsLength})`;
 });
 
 getMenuData(`${getDataAPI}filter.php?c=Seafood`).then((meals) => {
-  const itemsLength = countMenuItems(meals);
+  const mealsA = meals.meals;
+  const itemsLength = countMenuItems(mealsA);
   seaFood.innerText = `Sea Food (${itemsLength})`;
 });
 
 getMenuData(`${getDataAPI}filter.php?a=Canadian`).then((meals) => {
-  const itemsLength = countMenuItems(meals);
+  const mealsA = meals.meals;
+  const itemsLength = countMenuItems(mealsA);
   indianFood.innerText = `Candian Food (${itemsLength})`;
 });
 
+main.addEventListener('click', () => {
+  getMenuData(`${getDataAPI}search.php?f=b`).then((meals) => {
+    displayMenuItems(meals, homeList);
+  });
+  homeItems.innerHTML = '';
+  seaItems.innerHTML = '';
+  indianItmes.innerHTML = '';
+});
+
+allItems.addEventListener('click', () => {
+  getMenuData(`${getDataAPI}search.php?f=b`).then((meals) => {
+    displayMenuItems(meals, homeItems);
+  });
+  homeList.innerHTML = '';
+  seaItems.innerHTML = '';
+  indianItmes.innerHTML = '';
+});
+
 indianFood.addEventListener('click', () => {
-  homeItems.style.display = 'none';
-  seaItems.style.display = 'none';
   getMenuData(`${getDataAPI}filter.php?a=Canadian`).then((meals) => {
     displayMenuItems(meals, indianItmes);
   });
+  homeList.innerHTML = '';
+  homeItems.innerHTML = '';
+  seaItems.innerHTML = '';
 });
 
 seaFood.addEventListener('click', () => {
-  homeItems.style.display = 'none';
-  indianItmes.style.display = 'none';
   getMenuData(`${getDataAPI}filter.php?c=Seafood`).then((meals) => {
     displayMenuItems(meals, seaItems);
   });
+  homeList.innerHTML = '';
+  homeItems.innerHTML = '';
+  indianItmes.innerHTML = '';
 });
--- src/modules/counter.js
+++
@@ -1,6 +0,0 @@
-const countMenuItems = ((meals) => {
-  const mealArray = meals.meals;
-  return mealArray.length;
-});
-
-export default countMenuItems;
